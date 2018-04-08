e2e6715b1e26c222fb7f2197544c8c58c43846f6
ernests360naglis <36454603+ernests360naglis@users.noreply.github.com>
2018-04-08 16:46:03 +0200

    RCS WebProject

---
+++ RSCWebProject/WebProject.js
@@ -0,0 +1,70 @@
+// $('#btn-register').click(function(){
+    
+    
+    
+    
+    
+//     console.log($('.username').val());
+//     console.log($('.password').val());
+//     console.log($('.password-r').val());
+//     console.log($('.first-name').val());
+//     console.log($('.last-name').val());
+//     console.log($('.select-country').val());
+//     console.log($('.label-checkbox').val());
+
+
+
+//     function check(){
+//         var password = document.querySelector('.password').innerText;
+//         var passwordR = document.querySelector('.password-r').innerText;
+
+//             // password = parseInt(password);
+//             password = password.lenght;
+
+//             if(password >= 8){
+//                 password = document.querySelector('.password').innerText;
+
+//                 if(password == passwordR){
+//                     window.location = 'Chat.html';   
+//                 }else{
+//                     break;
+//                 }
+//             }
+//     }
+
+
+//     document
+//     .getElementById('btn-register').addEventListener('click', function(){
+//         // var incrementor = document.getElementById('incrementor').value;
+//         // incrementor = parseInt(incrementor);
+
+//         // var elements = document.querySelectorAll('.number');   //.number - (.) pasauc klasi
+//         // for (var i = 0; i < elements.length; i++) {
+//         //     var element = elements[i];
+//         //     var currentNumber = element.innerText;
+//         //     currentNumber = parseInt(currentNumber);
+//         //     element.innerText = currentNumber + incrementor;
+//         // }
+//     });
+
+//     // querySelector
+
+
+//     // var password = $('.password');
+//     // var passwordR = $('.password-r');
+//     // var passwordL = $('.password').lenght;
+//     // console.log(passwordL);
+//     // var passwordRL = $('.password-r').lenght;
+
+    
+//     // if (passwordL >= 8 && password == passwordR) {
+//     //     action="Chat.html";
+//     // }
+
+
+
+//     //  el1.html('foo <em>bar</em>');
+//      // el1.innerText = 'foo <em>bar</em>';
+//     //  el2.val('bar');
+// });
+
